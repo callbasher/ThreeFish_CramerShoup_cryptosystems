d17c32d84871e37b497907344211d155ec552095
callbasher <aurel12345@gmail.com>
2017-12-15 18:01:59 +0100

    function writefile done

--- src/ThreeFish.py
+++ src/ThreeFish.py
@@ -2,59 +2,55 @@
 # -*- coding: utf-8 -*-
 
 import random
+from src.Util import *
 
 # Constantes
-
-R = 49 # Décalage de R bits
-
-tweak0 = 99111103114097110110101  # premier tweak sur 64 bits (cogranne)
-
-tweak1 = 103115049053108111118101   # second tweak sur 64 bits (gs15love)
-
+# Décalage de R bits
+R = 49
+# premier tweak sur 64 bits (cogranne)
+tweak0 = 99111103114097110110101
+# second tweak sur 64 bits (gs15love)
+tweak1 = 103115049053108111118101
 tweak2 = tweak0 ^ tweak1
-
 tweaks = [tweak0, tweak1, tweak2]
-
-C = 513129967392069919254   # soit 0x1bd11bdaa9fc1a22 en hexa  # C = constante pour la génération des clés des tournées
+# soit 0x1bd11bdaa9fc1a22 en hexa C = constante pour la génération des clés des tournées
+C = 513129967392069919254
 
 # Création / génération de la clé symétrique
-
 def keygen(L_block):
     key = []
     hexkey = []
     k = 0
     for i in range(0, int(L_block/64)):
-        n = random.getrandbits(64)              # génération d'un int de 64bits
+        n = random.getrandbits(64)
         key.append(n)
-        k ^= n                                  # xor des int généré
+        k ^= n
 
-        hexk = hex(n)                           # convertion de l'int généré en hexa
-        hexk = hexk.replace('\'', '')           # supression des '
-        hexk = hexk.replace('0x', '', 1)        # suppression des caractères 0x au début du mot
-        hexk = str(hexk)
-
+        hexk = int2hexa(n)
+        # padding pour que le nombre convertis en hex soit sur 16bits
         if len(hexk) != 16:
-            pad = "0"                           # padding pour que le nombre convertis en hex soit sur 16bits
+            pad = "0"
             hexk = (16 - (len(str(hexk)))) * pad + str(hexk)
             hexkey.append(hexk)
         else:
             hexkey.append(hexk)
-
-        keyuser = ""     # keyuser est la clé symétrique que l'on va afficher à l'utilisateur
-        for i in hexkey:    # pour qu'il la note et s'ne serve pour le déchiffrement
+        # keyuser est la clé symétrique que l'on va afficher à l'utilisateur
+        keyuser = ""
+        # pour qu'il la note et s'ne serve pour le déchiffrement
+        for i in hexkey:
             keyuser += i
 
-    k ^= C          # xor de toutes les cles avec C puis ajout du résultat dans la clé
+    k ^= C
     key.append(k)
-    print(len(str(keyuser)))
     print("Voici votre clé symétrique sur ", L_block, " bits : \t\n######################################\t\n"
           , keyuser, "\t\n######################################")
+    # écriture de la clé symétrique dans un fichier
+    writefile("../test/resources/symKey.txt", keyuser)
     return key
 
 # Fin création / génération de la clé symétrique
 
 # Génération des 20 clés pour les tournées
-
 def keygenturn(key):
     N = len(key) - 1
     VingtKeys = []
@@ -91,10 +87,10 @@ def keygenturn(key):
        k ^= t
 
        k ^= C
-       tabKey.append(k)                                     # k est le dernier mot de 64 bits kn = k0+..+kn-1+C
+       # k est le dernier mot de 64 bits kn = k0+..+kn-1+C
+       tabKey.append(k)
 
        VingtKeys.append(tabKey)
     print(VingtKeys)
-    return VingtKeys         # var VingKeys est la var ou se trouve les 20 cles des tournées
-
+    return VingtKeys
 # Fin génération des clés
