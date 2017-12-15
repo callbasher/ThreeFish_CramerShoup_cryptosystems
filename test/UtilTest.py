8df7112deaa16c439f6fe543e6c32559857db9fa
callbasher <aurel12345@gmail.com>
2017-12-15 19:06:25 +0100

    unit test file update

--- test/UtilTest.py
+++ test/UtilTest.py
@@ -2,12 +2,8 @@ import pytest
 import binascii
 import os
 import sys
-sys.path.append('/home/dev/Documents/Projet_Cryptologie/src')
-from Util import *
+sys.path.append('C:/Users/aurélien/Google Drive/Cours UTT/STIC_3_SSI/CS_GS15_Chiffrement, signature électronique et PKI/Projet/Projet_Cryptologie/src')
+from src.Util import *
 
-# test lecture fichier
-def read_txt_file():
-    L_block = 256
-    fich = "resources/test.txt"
-    fichier = readfile(fich, L_block)
-    print(fichier)
+def testunit1():
+    assert 1 == 0
