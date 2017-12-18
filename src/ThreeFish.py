17aad491220285eff1b02d0a07a25a529b36775b
callbasher <aurel12345@gmail.com>
2017-12-18 19:10:04 +0100

    fonction inv_mix, ECB and tested on pytest done

--- src/ThreeFish.py
+++ src/ThreeFish.py
@@ -25,7 +25,6 @@ def keygen(L_block):
         n = random.getrandbits(64)
         key.append(n)
         k ^= n
-
         hexk = int2hexa(n)
         # padding pour que le nombre convertis en hex soit sur 16bits
         if len(hexk) != 16:
@@ -39,7 +38,6 @@ def keygen(L_block):
         # pour qu'il la note et s'ne serve pour le déchiffrement
         for i in hexkey:
             keyuser += i
-
     k ^= C
     key.append(k)
     print("Voici votre clé symétrique sur ", L_block, " bits : \t\n######################################\t\n"
@@ -47,7 +45,6 @@ def keygen(L_block):
     # écriture de la clé symétrique dans un fichier
     writefile("../test/resources/symKey.txt", keyuser)
     return key
-
 # Fin création / génération de la clé symétrique
 
 # Génération des 20 clés pour les tournées
@@ -61,7 +58,6 @@ def keygenturn(key):
           t = key[(i + n) % (N + 1)]
           tabKey.append(t)
           k ^= t
-
        # N - 3
        n = N - 3
        t = (key[(i + n) % (N + 1)]) + (tweaks[i % 3])
@@ -72,7 +68,6 @@ def keygenturn(key):
        tStr = tStr[len(tStr)-16:len(tStr)]
        t = int(tStr, 16)
        # cela calcul le modulo sans calculer 2^64 même si les puissances de 2 sont optimiser sur les processeur
-
        tabKey.append(t)
        k ^= t
        # N - 2
@@ -85,26 +80,69 @@ def keygenturn(key):
        t = (key[(i + n) % (N + 1)]) + i % (2**64)
        tabKey.append(t)
        k ^= t
-
        k ^= C
        # k est le dernier mot de 64 bits kn = k0+..+kn-1+C
        tabKey.append(k)
-
        VingtKeys.append(tabKey)
     return VingtKeys
 # Fin génération des clés
 
 # début fonction mélange
-def mixcolumn(m1 , m2, L_block):
+def mixcolumn(datalist):
     # en fonction de la taille du block on execute 2, 4 ou 8 fois le mélange
-    for i in range(0, (L_block / 128)):
-        m11 = (m1 ^ m2) % 2**64
-        m22 = m11 ^ (m2 << R)
-    return m11, m22
+    datalistmix = []
+    for i in range(0, len(datalist) - 1, 2):
+        m11 = (datalist[i] + datalist[i+1]) % 2**64
+        m22 = m11 ^ (datalist[i + 1] << R)
+        datalistmix.append(m11)
+        datalistmix.append(m22)
+    return datalistmix
 # Fin de la fonction mélange
+
+# début de la fonction inverse de mélange
+def inv_mixcolumn(datalist):
+    # en fonction de la taille du block on execute 2, 4 ou 8 fois le mélange
+    datalist_unmix = []
+    for i in range(0, len(datalist) - 1, 2):
+        m2 = (datalist[i] ^ datalist[i + 1]) >> R
+        m1 = (datalist[i] - m2) % 2**64
+        datalist_unmix.append(m1)
+        datalist_unmix.append(m2)
+    return datalist_unmix
+# Fin de la fonction inverse de mélange
 
 # début fonction de permutation
 def permute(n):
     # invertion de l'ordre des mots
     return list(reversed(n))
 # fin fonction de permutation
+
+# début fonction xor de la clé et du block qui est en train d'être chiffré
+def ajoutkey(i_block, i_tabkey):
+    datalistajoutkey = []
+    for i in range(0, len(i_block)):
+        datalistajoutkey.append((i_block[i] + i_tabkey[i]) % 2**64)
+    return datalistajoutkey
+# fin fonction xor de la clé et du block qui est en train d'être chiffré
+
+# chiffrement ECB début
+def ECBchiffThreef(datalist, tabkeys):
+    encryptdatalist = []
+    for j in datalist:
+        for i in range(0, 76):
+            # pour ajouter les clés toutes les 4 tournées
+            for r in range(0, 77, 4):
+                if i == r:
+                    # ajout de la clé
+                    datalist_ajoutkey = ajoutkey(j, tabkeys[int(i/4)])
+                    mixage = mixcolumn(datalist_ajoutkey)
+                    permutation = permute(mixage)
+                    j = permutation
+            mixage = mixcolumn(j)
+            permutation = permute(mixage)
+            j = permutation
+        # ajout de la dernière clé à la dernière tournée
+        datalist_ajoutfinalkey = ajoutkey(j, tabkeys[19])
+        encryptdatalist.append(datalist_ajoutfinalkey)
+    return encryptdatalist
+# chiffrement ECB fin
